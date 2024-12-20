340043b39baad9b135ccbb82410b04cf69be69a4
Busra Gazel <busragzl96@gmail.com>
2024-12-20 22:54:17 +0300

    Kullanıcı Kayıt Görünümünün oluşturulması Alert Mantığının denenmesi

--- AlertProject/ViewController.swift
+++ AlertProject/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userNameText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    @IBOutlet weak var pasword2Text: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func signUpButton(_ sender: Any) {
+        
+        let alert = UIAlertController(title: "Hata", message: "UserName not found", preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "Ok", style: UIAlertAction.Style.default) { UIAlertAction in
+            print("Button Clicked")
+        }
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil) // bir görünüm göstermeni istiyorum diyorum. (gösterilmek istenilen görünüm , animasyon olsun mu ? , Tamamlandıktan sonra bir işlem yapılacak mı? )
+        
+        
+    }
 }
 
