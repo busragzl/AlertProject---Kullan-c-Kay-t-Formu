0ef23ca0118d6b0223a65417961b8c934f0b4767
Busra Gazel <busragzl96@gmail.com>
2024-12-20 23:36:23 +0300

    Kullanıcı Giriş Kontrollerinin Eklenmesi

--- AlertProject/ViewController.swift
+++ AlertProject/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  AlertProject
 //
-//  Created by Musa on 20.12.2024.
+//  Created by Büşra Gazel on 20.12.2024.
 //
 
 import UIKit
@@ -20,7 +20,7 @@ class ViewController: UIViewController {
 
 
     @IBAction func signUpButton(_ sender: Any) {
-        
+        /*
         let alert = UIAlertController(title: "Hata", message: "UserName not found", preferredStyle: UIAlertController.Style.alert)
         let okButton = UIAlertAction(title: "Ok", style: UIAlertAction.Style.default) { UIAlertAction in
             print("Button Clicked")
@@ -28,7 +28,37 @@ class ViewController: UIViewController {
         alert.addAction(okButton)
         self.present(alert, animated: true, completion: nil) // bir görünüm göstermeni istiyorum diyorum. (gösterilmek istenilen görünüm , animasyon olsun mu ? , Tamamlandıktan sonra bir işlem yapılacak mı? )
         
+        */
         
+        if userNameText.text == "" {
+            
+            makeAlert(title: "Error", message: "Username not found")
+            
+        } else if passwordText.text == "" {
+            
+            makeAlert(title: "Error", message: "Password not found")
+           
+        } else if passwordText.text != pasword2Text.text {
+            
+            makeAlert(title: "Error", message: "Password not match")
+            
+        } else {
+            
+            makeAlert(title: "Success", message: "User OK")
+            
+        }
+            
     }
+    
+    
+    func makeAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
+    }
+        
+        
 }
+
 
